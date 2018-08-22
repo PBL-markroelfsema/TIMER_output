06eb511712d1ec88687e780d1ff4fdf5a9af41fb
Hel1vs <heleen.vansoest@pbl.nl>
2018-08-22 14:15:42 +0200

    Made my own setting project/working directory part, as I work in Mathijs' folder. But it doesn't seem to work properly now, when running Import-TIMER.

--- CD_LINKS/ReadTimerData_indicators.R
+++ CD_LINKS/ReadTimerData_indicators.R
@@ -1,18 +1,26 @@
 library(gridExtra)
 library(xlsx)
+
+#For Heleen, working in Mathijs' folder
+# ProjectDir = "~/disks/y/ontwapps/Timer/Users/Mathijs/Projects/CD-LINKS"
+# Project = "CD_LINKS"
+# R_dir = paste(ProjectDir, Project, "R-scripts/TIMER_output", sep="/")
+# setwd(R_dir)
+
+#For Mark
+ProjectDir = "~/disks/y/ontwapps/Timer/Users/Mark/"
+Project = "CD_LINKSupdate"
+R_dir = paste(ProjectDir, Project, "6_R/TIMER_output", sep="/")
+setwd(R_dir)
+getwd()
+
+# Source scripts (after setting working directory)
 source('functions/Settings.R')
 source('functions/General Functions.R')
 source('functions/Import_TIMER_output.R')
 source('functions/Process_TIMER_output.R')
 source('functions/pbl_colors.R')
 source('CD_LINKS/Settings_indicators.R')
-
-#ProjectDir = "~/disks/y/ontwapps/Timer/Users/Mathijs/Projects/CD-LINKS"
-ProjectDir = "~/disks/y/ontwapps/Timer/Users/Mark/"
-Project = "CD_LINKSupdate"
-R_dir = paste(ProjectDir, Project, "6_R/TIMER_output", sep="/")
-setwd(R_dir)
-getwd()
 
 # Read no policy scenario
 NoPolicy <- ImportTimerScenario('NoPolicy_update','NoPolicy_update')
@@ -28,12 +36,14 @@ write.table(var_names, "CD_LINKS/var_names.csv", sep=";", row.names=FALSE)
            
 # Read in other scenarios
 NPi <- ImportTimerScenario('NPi','NPi')
+#NPi_update <- ImportTimerScenario('NPi_update','NPi_update')
 INDCi <- ImportTimerScenario('INDCi','INDCi')
 NPi2020_1000 <- ImportTimerScenario('NPi2020_1000','NPi2020_1000')
 NPi2020_400<- ImportTimerScenario('NPi2020_400','NPi2020_400')
 INDCi2030_1000<- ImportTimerScenario('INDC2030i_1000','INDC2030i_1000')
 
 NPi_ind <- ProcessTimerScenario(NPi)
+#NPi_update_ind <- ProcessTimerScenario(NPi_update)
 INDCi_ind <- ProcessTimerScenario(INDCi)
 NPi2020_1000_ind <- ProcessTimerScenario(NPi2020_1000)
 NPi2020_400_ind <- ProcessTimerScenario(NPi2020_400)
